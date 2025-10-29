181d9ce71581675b3ed22e9e78d5564fda40e3eb
Ari Durlacher <arikodcode@gmail.com>
2025-10-29 10:28:35 +0200

    added fonction secret

--- game/secret.py
+++ game/secret.py
@@ -1,6 +1,15 @@
 import random
 
 
-def generate_secret() -> str:
+def generate_secret(length: int = 4, *, unique_digits: bool = True, allow_leading_zero: bool = False,
+                    rng: random.Random | None = None) -> str:
+    secret_nums = ""
+    list_nums = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    while len(secret_nums) != 4:
+        num = list_nums[random.randrange(len(list_nums))]
+        if num not in secret_nums:
+            secret_nums += num
+    return secret_nums
 
 
+print(generate_secret())
