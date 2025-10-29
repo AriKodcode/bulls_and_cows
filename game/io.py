7d9f823dcf7ca7eec22033cb409f4584d4fe91af
Ari Durlacher <arikodcode@gmail.com>
2025-10-29 12:14:14 +0200

    added new fonction print_status

--- game/io.py
+++ game/io.py
@@ -6,8 +6,8 @@ def print_feedback(guess: str, bulls: int, cows: int) -> None:
     print(f"guess: {guess}, bulls: {bulls}, cows: {cows}")
 
 
-def print_status() -> None:
-
+def print_status(state_game_state) -> None:
+    print(state_game_state)
 
 
 def print_result() -> None:
