2bb86724ea7d9a9be84182f6f3662a300390e50f
Ari Durlacher <arikodcode@gmail.com>
2025-10-29 12:13:02 +0200

    added new fonction print_feedback

--- game/io.py
+++ game/io.py
@@ -2,6 +2,8 @@ def prompt_guess() -> str:
     guess = input("Press 4 numbers between 1 - 9")
     return guess
 
+def print_feedback(guess: str, bulls: int, cows: int) -> None:
+    print(f"guess: {guess}, bulls: {bulls}, cows: {cows}")
 
 
 def print_status() -> None:
